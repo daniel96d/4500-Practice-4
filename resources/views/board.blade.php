8c5b08d80f302f94a49332a6693d6be43a3792e9
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-02 14:03:26 -0800

    update blade

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -14,7 +14,66 @@
             <h3 class="card-title"> Backlog </h3>
             ::after
         </div>
-      </div>
+        <div call="card-body">
+          <div class="card card-info card-outline">
+            <div class="card-header">
+              <h5 class="card-title">Create Labels</h5>
+              <div class="card-tools">
+                <a href="#" class="btn btn-tool btn-link">#3</a>
+                <a href="#" class="btn btn-tool">
+                  <i class="fas fa-pen">
+                    ::before
+                  </i>
+                </a>
+              </div>
+              ::after
+            </div class="card-body">
+            <div class="custom-controll custom-checkbox">
+              <input class="custom-control-input" type="checkbox"
+              id="customCheckbox1" disabled>
+              <label for="customCheckbox1" class="custom-controllabel">
+                ::before
+                "BUG"
+                ::after
+              </label>
+            </div>
+            <div class="custom-controll custom-checkbox">
+              <input class="custom-control-input" type="checkbox"
+              id="customCheckbox2" disabled>
+              <label for="customCheckbox2" class="custom-controllabel">
+                ::before
+                "Feature"
+                ::after
+              </label>
+            </div>
+            <div class="custom-controll custom-checkbox">
+              <input class="custom-control-input" type="checkbox"
+              id="customCheckbox3" disabled>
+              <label for="customCheckbox3" class="custom-controllabel">
+                ::before
+                "Enhancement"
+                ::after
+              </label>
+            </div>
+            <div class="custom-controll custom-checkbox">
+              <input class="custom-control-input" type="checkbox"
+              id="customCheckbox4" disabled>
+              <label for="customCheckbox4" class="custom-controllabel">
+                ::before
+                "Documentation"
+                ::after
+              </label>
+            </div>
+            <div class="custom-controll custom-checkbox">
+              <input class="custom-control-input" type="checkbox"
+              id="customCheckbox5" disabled>
+              <label for="customCheckbox5" class="custom-controllabel">
+                ::before
+                "Examples"
+                ::after
+              </label>
+            </div>
+
     </div>
 @stop
 
