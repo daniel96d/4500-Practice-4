e5d5f3cd67940fe76d84e52c5a2c07a90099ef10
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-02 13:49:57 -0800

    update

--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -8,6 +8,14 @@
 
 @section('content')
     <p>Content here</p>
+    <div class="col-md-3 col-sm-6 col-12">
+      <div class="card card-row card-secondary">
+        <div class="card-header">
+            <h3 class="card-title"> Backlog </h3>
+            ::after
+        </div>
+      </div>
+    </div>
 @stop
 
 @section('css')
