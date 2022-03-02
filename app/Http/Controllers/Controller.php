7ca910d74aeb11a94c999484d72ac9f7fdeec09d
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-02 12:05:23 -0800

    initial upload

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+//php artisan make:controller AssignmentController
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
