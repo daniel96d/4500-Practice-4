aafebb9af610d6a3b210d15576073bafaf36f270
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-03 13:14:14 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,8 @@ Route::get('/admin/board', function () {
 });
 Route::get('/events-feed', function () {
     $events = array(
-      ["title"=>"CSE4500 Class","start"=>"2022-02-23T17:30:00","end"=>"2022-02-23T18:45:00"],
-      ["title"=>"CSE4500 Class","start"=>"2022-02-28T17:30:00","end"=>"2022-02-28T18:45:00"]
+      array("title"=>"CSE4500 Class","start"=>"2022-02-23T17:30:00","end"=>"2022-02-23T18:45:00"),
+      array("title"=>"CSE4500 Class","start"=>"2022-02-28T17:30:00","end"=>"2022-02-28T18:45:00")
     );
     return json_encode($events);
 });
@@ -53,6 +53,7 @@ Route::fallback(function () {
     return view('emergency');
 });
 
+//Database routes\\
 Route::get('/db-test',function () {
   try{
     \DB::connection()->getPDO();
