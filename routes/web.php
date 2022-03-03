ba35d6b05a0844ffc5ad3cb43b324ffc8f6c27ee
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-02 19:02:13 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,8 @@ Route::get('/admin/board', function () {
     return view('board');
 });
 Route::get('/events-feed', function () {
-    $events = array("title"=>"CSE4500 Class","start"=>"2022-02-23T17:30:00","end"=>"2022-02-23T18:45:00");
+    $events = array(array("title"=>"CSE4500 Class","start"=>"2022-02-23T17:30:00",
+    "end"=>"2022-02-23T18:45:00"));
     return json_encode($events);
 });
 Route::fallback(function () {
