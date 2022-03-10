1343808d26ed76543693990d78f20742e5969a3e
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-10 12:36:26 -0800

    Update create.blade.php

--- resources/views/calendar/create.blade.php
+++ resources/views/calendar/create.blade.php
@@ -11,6 +11,11 @@
     @csrf
     <x-adminlte-input name="title" label="Title" />
 
+    <label for="meeting-time">Choose a time for your event:</label>
+    <input type="datetime-local" id="meeting-time"
+       name="meeting-time" value="2018-06-12T19:30"
+       min="2018-06-07T00:00" max="2018-06-14T00:00">
+
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
