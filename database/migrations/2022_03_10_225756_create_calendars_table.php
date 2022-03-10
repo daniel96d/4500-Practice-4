f4dc6d7a13b59ed88c643af683dfbe12a927b2cf
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-10 15:03:05 -0800

    Update 2022_03_10_225756_create_calendars_table.php

--- database/migrations/2022_03_10_225756_create_calendars_table.php
+++ database/migrations/2022_03_10_225756_create_calendars_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-      Schema::create('calendar', function (Blueprint $table) {
+      Schema::create('calendars', function (Blueprint $table) {
           $table->id();
           $table->timestamps();
           $table->string('title');
