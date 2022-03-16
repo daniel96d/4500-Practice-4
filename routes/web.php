1c58033f3c9a38a130b0e58b32cbfc80487450d2
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-03-16 15:12:55 -0700

    update

--- routes/web.php
+++ routes/web.php
@@ -44,20 +44,6 @@ Route::get('/board', function () {
     return view('board');
 });
 Route::get('/events-feed', function () {
-    /*$events = array(
-      array(
-        "title"=>"CSE4500 Class",
-        "start"=>"2022-02-23T17:30:00",
-        "end"=>"2022-02-23T18:45:00"
-      ),
-      array(
-        "title"=>"CSE4500 Class",
-        "start"=>"2022-02-28T17:30:00",
-        "end"=>"2022-02-28T18:45:00"
-      )
-    );*/
-
-    //foreach(Calendar::all() as $events)
 
     $events = Calendar::select(['title', 'startTime AS start', 'endTime AS end'])->get();
 
